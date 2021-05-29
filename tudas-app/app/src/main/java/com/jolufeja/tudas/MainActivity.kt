2159524495a06941f97ff03dae46ea13c0a47691
JankaDeldok <hartmannjanka@gmail.com>
2021-05-29 10:25:45 +0200

    Adapted navigation bar and integrated profile page

--- tudas-app/app/src/main/java/com/jolufeja/tudas/MainActivity.kt
+++ tudas-app/app/src/main/java/com/jolufeja/tudas/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView
                 >(R.id.bottom_navigatin_view)
         val navController = findNavController(R.id.nav_fragment)
-        bottomNavigationView.setupWithNavController(navController
-        )
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
