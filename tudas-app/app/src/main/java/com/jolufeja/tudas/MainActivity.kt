0f93fb2bf9474d59934ec0ff572b8375739e91c5
Lukas Rieger <lukasrieger07@gmail.com>
2021-05-29 20:22:55 +0200

    Refactor fragment classes and use Koin DI for passing arguments to Fragments.

--- tudas-app/app/src/main/java/com/jolufeja/tudas/MainActivity.kt
+++ tudas-app/app/src/main/java/com/jolufeja/tudas/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.jolufeja.tudas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.koin.androidx.fragment.android.setupKoinFragmentFactory
+import org.koin.core.KoinExperimentalAPI
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    @KoinExperimentalAPI
     override fun onCreate(savedInstanceState: Bundle?) {
+        setupKoinFragmentFactory()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView
-                >(R.id.bottom_navigatin_view)
+        val bottomNavigationView =
+            findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
         val navController = findNavController(R.id.nav_fragment)
-        bottomNavigationView.setupWithNavController(navController
-        )
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
