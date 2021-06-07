0b2273ee6761ea7809ea9e6ffdf86027da4662ac
Lukas Rieger <lukasrieger07@gmail.com>
2021-06-07 14:07:35 +0200

    wip.

--- tudas-app/app/src/main/java/com/jolufeja/tudas/RegistrationViewModel.kt
+++ tudas-app/app/src/main/java/com/jolufeja/tudas/RegistrationViewModel.kt
@@ -1,9 +1,20 @@
 package com.jolufeja.tudas
 
-import androidx.lifecycle.ViewModel
-import com.jolufeja.authentication.UserAuthenticationService
 
+import com.jolufeja.authentication.UserAuthenticationService
+import com.jolufeja.authentication.registration.RegistrationError
+
+import com.jolufeja.httpclient.error.ErrorHandler
+import com.jolufeja.presentation.viewmodel.FetcherViewModel
+
+private val RegistrationErrorHandler = ErrorHandler { RegistrationError.RegistrationFailed(it) }
 
 class RegistrationViewModel(
     private val authenticationService: UserAuthenticationService
-) : ViewModel()
+) : FetcherViewModel<RegistrationError, Unit>(autoLoad = false) {
+
+    override suspend fun fetchData() {
+        TODO("Not yet implemented")
+    }
+
+}
