7d41525d461dc12257c90866826c38106f6fa515
Jonathan <Jonathan.Haudenschild@gmail.com>
2021-06-17 00:55:54 +0200

    Refactored and changed small things

--- tudas-app/app/src/main/java/com/jolufeja/tudas/adapters/RecycleViewAdapter.kt
+++ tudas-app/app/src/main/java/com/jolufeja/tudas/adapters/RecycleViewAdapter.kt
@@ -4,18 +4,23 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.jolufeja.tudas.R
+import com.jolufeja.tudas.data.ChallengesItem
 import com.jolufeja.tudas.data.ListItem
 import java.util.*
 
-class FeedRecycleViewAdapter(
+
+// Adapter to create a challenge card
+class ChallengesRecycleViewAdapter(
     private val context: Context,
     private val mDataList: ArrayList<ListItem>,
     val layoutCard: Int,
-    val layoutHeader: Int
+    val layoutHeader: Int,
+    private val listener: (ListItem) -> Unit
 ) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -37,10 +42,10 @@ class FeedRecycleViewAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (mDataList[position].getType()) {
             0 -> {
-                (holder as CardViewHolder).bind(position)
+                (holder as ChallengesRecycleViewAdapter.CardViewHolder).bind(position)
             }
             1 -> {
-                (holder as HeaderViewHolder).bind(position)
+                (holder as ChallengesRecycleViewAdapter.HeaderViewHolder).bind(position)
             }
         }
     }
@@ -53,17 +58,34 @@ class FeedRecycleViewAdapter(
         return mDataList[position].getType()
     }
 
+
     private inner class CardViewHolder(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
-        var text: TextView = itemView.findViewById(R.id.feed_text) as TextView
-        var cardFrameLayout: FrameLayout = itemView.findViewById(R.id.card_feed) as FrameLayout
+        var title: TextView = itemView.findViewById<View>(R.id.challenge_title) as TextView
+        var author: TextView =
+            itemView.findViewById<View>(R.id.challenge_author) as TextView
+        var timeLeft: TextView =
+            itemView.findViewById<View>(R.id.challenge_time_left) as TextView
+        var description: TextView =
+            itemView.findViewById<View>(R.id.challenge_description) as TextView
+        var reward: TextView =
+            itemView.findViewById<View>(R.id.challenge_reward) as TextView
+        var points: TextView =
+            itemView.findViewById<View>(R.id.challenge_points) as TextView
+        var button: Button = itemView.findViewById<View>(R.id.challenge_button) as Button
 
         fun bind(position: Int) {
             val recyclerViewModel = mDataList[position]
-            text.text = recyclerViewModel.text
-            val androidColors : IntArray = context.resources.getIntArray(R.array.colorarray)
-            val randomAndroidColor: Int = androidColors[Random().nextInt(androidColors.size)]
-            cardFrameLayout.background.setTint(randomAndroidColor)
+
+            title.text = recyclerViewModel.title
+            author.text = recyclerViewModel.author
+            timeLeft.text = recyclerViewModel.timeLeft.toString();
+            reward.text = recyclerViewModel.reward
+            points.text = recyclerViewModel.points.toString();
+            description.text = recyclerViewModel.description
+
+            // OnClick Listener on Button
+            button.setOnClickListener { listener(recyclerViewModel) }
         }
     }
 
@@ -75,6 +97,5 @@ class FeedRecycleViewAdapter(
             text.text = recyclerViewModel.text
         }
     }
-
 
 }
