16e920bcf9c48552fa46a9a2984a3f4ae713de84
JankaDeldok <hartmannjanka@gmail.com>
2021-06-30 11:02:48 +0200

    build in notification

--- tudas-app/app/src/main/java/com/jolufeja/tudas/MainActivity.kt
+++ tudas-app/app/src/main/java/com/jolufeja/tudas/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.jolufeja.tudas
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -20,6 +28,9 @@ import org.koin.androidx.fragment.android.setupKoinFragmentFactory
 import org.koin.core.KoinExperimentalAPI
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val CHANNEL_ID = "channel_id_test_01"
+    private val notificationId = 101
 
     private val navigationEventBus: NavigationEventBus by inject()
 
@@ -70,5 +81,41 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
         register(LoginNavigationEvents.PROCEED_TO_HOME, R.id.nav_graph_authenticated)
         register(LoginNavigationEvents.PROCEED_TO_REGISTRATION, R.id.registrationFragment)
+    }
+
+    fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "TUDAS Notification Channel"
+            val descriptionText = "This is the Channel for all TUDAS Notifications"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun sendNotification(title: String, description: String) {
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(title)
+            .setContentText(description)
+            .setContentIntent((pendingIntent))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        builder.setAutoCancel(true);
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build())
+        }
     }
 }
--- tudas-app/app/src/main/java/com/jolufeja/tudas/ProfileFragment.kt
+++ tudas-app/app/src/main/java/com/jolufeja/tudas/ProfileFragment.kt
@@ -1,18 +1,28 @@
 package com.jolufeja.tudas
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        (activity as MainActivity).createNotificationChannel()
 
         val profileFragment = ProfileFragment()
 
@@ -30,6 +40,9 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
         // log out Button
         var logOutButton: Button = view.findViewById<View>(R.id.logOutButton) as Button
+
+        // test notifications Button
+        var testNotificationsButton: Button = view.findViewById<View>(R.id.testNotificationsButton) as Button
 
         //opens FriendsSettingsFragment when clicked, but no layout yet
         friendsButton.setOnClickListener{
@@ -84,6 +97,10 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
         logOutButton.setOnClickListener {
             //logging out
+        }
+
+        testNotificationsButton.setOnClickListener {
+            (activity as MainActivity).sendNotification("Test Notification", "This is a test notification")
         }
     }
 }
