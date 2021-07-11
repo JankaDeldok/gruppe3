11532605f5a0a83c7efaad9363ac4d0b4c58e995
JankaDeldok <hartmannjanka@gmail.com>
2021-07-11 16:17:54 +0200

    added layout to add friends

--- tudas-app/app/src/main/java/com/jolufeja/tudas/ChangeEmailFragment.kt
+++ tudas-app/app/src/main/java/com/jolufeja/tudas/ChangeEmailFragment.kt
@@ -16,13 +16,10 @@ class ChangeEmailFragment(
 ) : Fragment(R.layout.fragment_change_email) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        // Save Button
         var saveButton: Button = view.findViewById<View>(R.id.save) as Button
 
-        // Back Button
         var cancelButton: Button = view.findViewById<View>(R.id.cancel) as Button
 
-        // Listener for Back Button to close fragment
         cancelButton.setOnClickListener {
             requireActivity().supportFragmentManager.popBackStack();
         }
